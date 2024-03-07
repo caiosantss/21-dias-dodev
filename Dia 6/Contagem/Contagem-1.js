1a4e3425876ce38d96591af983956ec01b92a492
Caio Santos <caiosilvasantos87@gmail.com>
2024-03-07 11:33:45 -0300

    Usando for e loop

---
+++ Dia 6/Contagem/Contagem-1.js
@@ -0,0 +1,9 @@
+// Contagem até o numero que a pessoa desejar de 1 em 1.
+
+let numero = Number(prompt("Até que numero vocé deseja que seja realizada a contagem?"))
+
+for (let contador = 0 ; contador <= numero; contador++){
+    console.log(contador)
+}
+
+
---
+++ Dia 6/Tabuada/tabuada.js
@@ -0,0 +1,9 @@
+
+//tabuada de numero pedido 
+
+let tabuada = Number(prompt("De qual numero você deseja saber a tabuada?"))
+
+for(let numero = 0; numero <= 10; numero++){
+ console.log(tabuada + " x " + numero + " = " + tabuada*numero)   
+}
+
